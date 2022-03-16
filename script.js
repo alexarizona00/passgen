ae5b9864dfd67aef208a2978fdebe66519d8c90c
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-16 16:42:17 -0700

    added variables for all the potential characters for password

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+let lilLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let symbols = ["!", "@" , "#","$","%","^","&","*","(",")"]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
