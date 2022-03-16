0027c41307263a110e5455cf9825f648f6871f03
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-16 16:53:57 -0700

    started work on the generatePassword function. added prompt for length and confirms to include different character types

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let lilLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 let symbols = ["!", "@" , "#","$","%","^","&","*","(",")"]
+
+
+function generatePassword(){
+let passLength = prompt("How long of a password do you need to generate?")
+let useCaps = confirm("Press OK to include Capital letters")
+let useNumeros = confirm("Press OK to include numbers")
+let useSymbols = confirm("Press OK to include symbols")
+
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
