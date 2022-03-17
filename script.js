19a1ebc8384a61e5763a2418840cfbf33f32b577
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-16 17:30:57 -0700

    devised a way to concat the chosen arrays into one so the for loop only chooses from characters requested by the user

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-// Assignment Code
+// Assignment Code by AA
 var generateBtn = document.querySelector("#generate");
-let lilLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let lilLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] // AA
 let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let symbols = ["!", "@" , "#","$","%","^","&","*","(",")"]
+let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"] //AA
+let usableArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-function generatePassword(){
-let passLength = prompt("How long of a password do you need to generate?")
-let useCaps = confirm("Press OK to include Capital letters")
-let useNumeros = confirm("Press OK to include numbers")
-let useSymbols = confirm("Press OK to include symbols")
+function generatePassword() {
+  let passLength = prompt("How long of a password do you need to generate?")
+  let useCaps = confirm("Press OK to include Capital letters")//AA
+  let useNumeros = confirm("Press OK to include numbers")
+  let useSymbols = confirm("Press OK to include symbols")
 
 
+
+  console.log(passLength)
+  console.log(useCaps)
+  console.log(useNumeros)
+  console.log(useSymbols)
+
+  if (useCaps) {
+    usableArray = usableArray.concat(bigLetters)
+  }
+
+  if (useNumeros) {
+    usableArray = usableArray.concat(numeros)
+  }
+
+  if (useSymbols) {
+    usableArray = usableArray.concat(symbols)
+  }
+
+  console.log(usableArray)
+
+
+  
 
 }
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input madebyAA
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
-}
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+  // Add event listener to generate button made by AA
+  generateBtn.addEventListener("click", writePassword);
