8fea2ab99360ecf8afc1326b3d143e4a27ab7827
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-16 18:07:04 -0700

    finally worked out the for loop to go through usable character array and store result in the finaly variable. then have it return that

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"] //AA
 let usableArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let thePass = [];
 
 
 function generatePassword() {
@@ -35,18 +36,26 @@ function generatePassword() {
   console.log(usableArray)
 
 
-  
+   for (i = 0; i < passLength; i++) {
+    thePass +=  usableArray[Math.floor(Math.random() * usableArray.length)]
+
+    }
+
+
+
+console.log(thePass)
+  return thePass
 
 }
-  // Write password to the #password input madebyAA
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+// Write password to the #password input  AA 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+  passwordText.value = password;
 
-  }
+}
 
 
-  // Add event listener to generate button made by AA
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button  AA
+generateBtn.addEventListener("click", writePassword);
