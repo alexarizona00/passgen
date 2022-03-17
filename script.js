f5b89791568a0ae0fc6d881fbdfb8144041bcdfd
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-16 19:02:26 -0700

    changed my html doc to make the tab have different title

--- script.js
+++ script.js
@@ -3,67 +3,67 @@ var generateBtn = document.querySelector("#generate");
 let lilLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] // AA
 let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")","!", "@", "#", "$", "%", "^", "&", "*", "(", ")"] //AA
+let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"] //AA
 let usableArray = [];
 let thePass = [];
 
 
 function generatePassword() {
   let passLength = prompt("How long of a password do you need to generate? \nIt must be between 8 - 128 characters")
-  if(passLength < 8 ){
+  if (passLength < 8) {
     alert("You need a minimum of 8 characters for your password!")
-      return thePass     
+    return thePass
   }
-  else if(passLength > 128){
+  else if (passLength > 128) {
     alert("Your password cannot be more than 128 characters!")
-      return thePass    
+    return thePass
   }
-  else{
-  let useLower = confirm("Press OK to include lowercase letters");
-  let useCaps = confirm("Press OK to include Capital letters");//AA
-  let useNumeros = confirm("Press OK to include numbers");
-  let useSymbols = confirm("Press OK to include symbols");
-  
-  
+  else {
+    let useLower = confirm("Press OK to include lowercase letters");
+    let useCaps = confirm("Press OK to include Capital letters");//AA
+    let useNumeros = confirm("Press OK to include numbers");
+    let useSymbols = confirm("Press OK to include symbols");
 
 
-  console.log(passLength)
-  console.log(useCaps)
-  console.log(useNumeros)
-  console.log(useSymbols)
-// these if statements check to see what chars they want in their password and adds them to an array that the for loop will eventually iterate through
 
-if (!useSymbols && !useCaps && !useNumeros && !useLower){
-  alert("You need to choose at least one character type to include in your password")
-      return thePass   
 
-}
-  if (useSymbols) {
-    usableArray = usableArray.concat(symbols)
-    
+    console.log(passLength)
+    console.log(useCaps)
+    console.log(useNumeros)
+    console.log(useSymbols)
+    // these if statements check to see what chars they want in their password and adds them to an array that the for loop will eventually iterate through
+
+    if (!useSymbols && !useCaps && !useNumeros && !useLower) {
+      alert("You need to choose at least one character type to include in your password")
+      return thePass
+
+    }
+    if (useSymbols) {
+      usableArray = usableArray.concat(symbols)
+
+    }
+    if (useCaps) {
+      usableArray = usableArray.concat(bigLetters)
+    }
+    if (useNumeros) {
+      usableArray = usableArray.concat(numeros)
+    }
+    if (useLower) {
+      usableArray = usableArray.concat(lilLetters)
+    }
+
+    console.log(usableArray)
+
+    for (i = 0; i < passLength; i++) {
+      thePass += usableArray[Math.floor(Math.random() * usableArray.length)]
+
+    }
   }
-  if (useCaps) {
-    usableArray = usableArray.concat(bigLetters)
-  }
-  if (useNumeros) {
-    usableArray = usableArray.concat(numeros)
-  }
-  if (useLower) {
-    usableArray = usableArray.concat(lilLetters)
-  }
- 
-  console.log(usableArray)
 
-  for (i = 0; i < passLength; i++) {
-    thePass += usableArray[Math.floor(Math.random() * usableArray.length)]
-
-  }
-}
-
-// this stores the password in another variable to return, so that we can reset the variables to initial values in case they run app back to back
-let displayedPass = thePass;
-thePass = [];
-usableArray= []; 
+  // this stores the password in another variable to return, so that we can reset the variables to initial values in case they run app back to back
+  let displayedPass = thePass;
+  thePass = [];
+  usableArray = [];
 
 
   return displayedPass;
